95177b64e47d7b261c0b7bc1443ab80971b87486
George Stavrinos <stavrinosgeo@gmail.com>
2018-10-10 20:01:40 +0300

    Working version for Ubuntu 18.04. I need to check if it is also working for Ubuntu 16.04 so I can merge to master

--- multikill.py
+++ multikill.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python2
-import os
-import gtk
+from gi.repository import Gtk, Gdk
+#import gtk
 import signal
 import terminatorlib.plugin as plugin
 from terminatorlib.util import err, dbg
@@ -15,23 +14,34 @@ class MultiKill(plugin.MenuItem):
 
     def __init__(self):
         plugin.MenuItem.__init__(self)
+        err('\033[1;31m 1 \033[0m')
         self.entry = Terminator().windows[0]
+        err('\033[1;31m 2 \033[0m')
         self.entry.connect('key-release-event', self.onKeyPress)
+        err('\033[1;31m 3 \033[0m')
         
 
     def callback(self, menuitems, menu, terminal):
-        item = gtk.MenuItem(_('MultiKill!'))
+        err('\033[1;31m 4 \033[0m')
+        item = Gtk.MenuItem(_('MultiKill!'))
+        err('\033[1;31m 5 \033[0m')
         item.connect("activate", self.multiKill)
+        err('\033[1;31m 6 \033[0m')
         menuitems.append(item)
+        err('\033[1;31m 7 \033[0m')
 
     def multiKill(self, widget):
+        err('\033[1;31m 8 \033[0m')
         for t in Terminator().terminals:
+            err('\033[1;31m 9 \033[0m')
             try:
-                t.vte.feed_child("\x03")
+                t.vte.feed_child("\x03", len("\x03"))
             except Exception, ex:
-                dbg('\033[1;31mMultikill failed: %s\033[0m' % ex)
+                err('\033[1;31mMultikill failed: %s\033[0m' % ex)
                 pass
 
     def onKeyPress(self, widget, event):
-        if (event.state & gtk.gdk.MOD1_MASK == gtk.gdk.MOD1_MASK) and (event.keyval == 67 or event.keyval == 99): # Alt+C or Alt+c
+        err('\033[1;31m 666 \033[0m')
+        if (event.state & Gdk.ModifierType.MOD1_MASK == Gdk.ModifierType.MOD1_MASK) and (event.keyval == 67 or event.keyval == 99): # Alt+C or Alt+c
+            err('\033[1;31m 777 \033[0m')
             self.multiKill(widget)
