0275d4e1f3fb11fc6f6e784510828a2fa942ddbf
George Stavrinos <gstavrinos@iit.demokritos.gr>
2018-10-10 20:37:03 +0300

    Changes for 16.04 compatibility

--- multikill.py
+++ multikill.py
@@ -1,10 +1,17 @@
-from gi.repository import Gtk, Gdk
-#import gtk
 import signal
 import terminatorlib.plugin as plugin
 from terminatorlib.util import err, dbg
 from terminatorlib.translation import _
 from terminatorlib.terminator import Terminator
+from terminatorlib.version import APP_VERSION
+
+if float(APP_VERSION) <= 0.98:
+    import gtk as Gtk
+else:
+    import gi
+    from gi.repository import Gtk, Gdk
+
+err('\033[1;31m %s \033[0m' % APP_VERSION)
 
 # AVAILABLE must contain a list of all the classes that you want exposed
 AVAILABLE = ['MultiKill']
@@ -42,6 +49,11 @@ class MultiKill(plugin.MenuItem):
 
     def onKeyPress(self, widget, event):
         err('\033[1;31m 666 \033[0m')
-        if (event.state & Gdk.ModifierType.MOD1_MASK == Gdk.ModifierType.MOD1_MASK) and (event.keyval == 67 or event.keyval == 99): # Alt+C or Alt+c
-            err('\033[1;31m 777 \033[0m')
-            self.multiKill(widget)
+        if float(APP_VERSION) <= 0.98:
+            if (event.state & Gtk.gdk.MOD1_MASK == Gtk.gdk.MOD1_MASK) and (event.keyval == 67 or event.keyval == 99): # Alt+C or Alt+c
+                err('\033[1;31m 777 \033[0m')
+                self.multiKill(widget)
+        else:
+            if (event.state & Gdk.ModifierType.MOD1_MASK == Gdk.ModifierType.MOD1_MASK) and (event.keyval == 67 or event.keyval == 99): # Alt+C or Alt+c
+                err('\033[1;31m 777 \033[0m')
+                self.multiKill(widget)
